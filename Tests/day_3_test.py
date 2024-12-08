f1e41615466f4722452ba312c2dfa235c368302b
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2024-12-07 20:02:26 -0700

    day_3_finished

--- Tests/day_3_test.py
+++ Tests/day_3_test.py
@@ -1,10 +1,17 @@
 import unittest
-from day_3 import part_one
+from day_3 import part_one,part_two,jen_part_two
 
 class day_3_tests(unittest.TestCase):
-    
+
     def test_given(self):
         testcase = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
         self.assertEqual(161,part_one(testcase))
-        
+
+    def test_pt2(self):
+        testInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        self.assertEqual(48, part_two(testInput))
+
+    def test_jen_part_two(self):
+        testInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        self.assertEqual(48,jen_part_two(testInput))
 
--- day_3.py
+++ day_3.py
@@ -9,6 +9,8 @@ def main():
             string = message.readline()
 
     #print(part_one(input_string))
+    print(part_two(input_string))
+    print(jen_part_two(input_string))
 
 def part_one(input:str):
     list_of_mult = re.findall("mul[(][0-9]+,[0-9]+[)]",input) #mul(num,num)
@@ -19,6 +21,34 @@ def part_one(input:str):
     return sum
 
 def part_two(input: str):
+    do_list = re.split("(do[(][)])|(don't[(][)])", input)
+    status = True
+    #have we encountered a do or don't
+    sum = 0
+    for ticket in do_list:
+        if ticket == None:
+            continue
+        if ticket == "do()":
+            status = True
+        if ticket == "don't()":
+            status = False
+        if status: 
+            sum += part_one(ticket)
 
+    return sum
+
+def jen_part_two(input:str) -> int:
+    list_of_mult = re.findall("mul[(][0-9]+,[0-9]+[)]|do[(][)]|don't[(][)]",input)
+    status = True
+    sum = 0
+    for ticket in list_of_mult:
+        if ticket == "do()":
+            status = True
+        elif ticket == "don't()":
+            status = False
+        elif status: 
+            nums = re.match("mul[(]([0-9]+),([0-9]+)[)]",ticket)
+            sum += int(nums.group(1)) * int(nums.group(2))
+    return sum
 if __name__ == '__main__':
     main()
